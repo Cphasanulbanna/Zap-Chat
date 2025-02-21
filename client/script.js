bb56b20f7a014918acc3ab92a5b9b5865e629e99
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-21 12:38:05 +0530

    added socket cdn

--- client/script.js
+++ client/script.js
@@ -1,7 +1,23 @@
-import { io } from "socket.io-client";
+import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
 
 const socket = io("http://localhost:3000")
 
 socket.on("connect", () => {
     console.log(`Connected to the server with id ${socket.id}`);
+})
+
+const chatForm = document.getElementById("chat-form")
+const messageInput = document.getElementById("message-input")
+
+
+chatForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const message = messageInput.value
+    if(message === "") return
+
+    socket.emit("send-message", message)
+    messageInput.value = ""
+
+
 })
--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
-const io = require('socket.io')(3000, {
-    cors:  {
-        origin: ["https://localhost:5500"]
+const { Server } = require("socket.io");
+
+const io = new Server(3000, {
+    cors: {
+        origin: ["http://127.0.0.1:5500"], // frontend origin
     }
 })
 
 io.on("connection", (socket) => {
     console.log(socket.id); //random id generated for each person when connected
+
+    socket.on("send-message", (message) => {
+        console.log(message, "message");
+        
+    })
     
 })
