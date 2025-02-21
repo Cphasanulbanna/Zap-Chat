fd79a3e674a2d30d06dac73643b9c0db836bfc5c
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-21 12:12:19 +0530

    chat screen ui

--- client/script.js
+++ client/script.js
@@ -1,3 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3000")
+
+socket.on("connect", () => {
+    console.log(`Connected to the server with id ${socket.id}`);
+})
